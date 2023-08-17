665138bf5e360b6f0372dd12e0609d936d544a08
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-17 06:07:56 +0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative 'message_outputs'
 require_relative 'class_genre_handler'
 require_relative 'class_musicalbum_handler'
+require_relative 'class_storage'
 
 class App
   include MessageOutputs
@@ -9,6 +10,7 @@ class App
   def initialize
     @musicalbum_handler = MusicalbumHandler.new
     @genre_handler = GenreHandler.new
+    @music_storage = Storage.new
     @music_albums = [] # Initialize the instance variable here
   end
 
@@ -18,6 +20,9 @@ class App
       number_choice = options
       option_selected(number_choice)
     end
+  ensure
+    save_genres_json
+    save_albums_json
   end
 
   def option_selected(number)
@@ -34,5 +39,21 @@ class App
     else
       puts 'Invalid number: Please enter a valid number'
     end
+  end
+
+  def save_genres_json
+    @music_storage.save_genres_json(@genre_handler.genres) # Call correct method
+  end
+
+  def load_genres_json
+    @music_storage.load_genres_json(@genre_handler.genres) # Call correct method
+  end
+
+  def load_albums_json
+    @music_storage.load_musicalbum_json(musicalbum_handler.musicalbums)
+  end
+
+  def save_albums_json
+    @music_storage.save_musicalbum_json(musicalbum_handler.musicalbums)
   end
 end
